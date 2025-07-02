d3c5a7420c40420fd2b709ee32d82908434e0636
Mohammed Bin Yahiya <129117645+mx7h@users.noreply.github.com>
2025-07-02 20:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function isSameType(value1, value2) {
 		if(typeof value1 ===  typeof value2 )
 		{
 			return true
+		}else if(typeof value1 == NaN && typeof value2 ==NaN){
+			return true
 		}else if(typeof value1 !==  typeof value2 )
 		{
 			return false
